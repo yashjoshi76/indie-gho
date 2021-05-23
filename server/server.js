1d2ee334bf10e8c8fef732a1531c55d00915b053
Yash Joshi <yashjoshi@Yashs-MacBook-Air.local>
2021-05-23 18:19:31 +0530

    fixing

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 console.log(process.env.REDIRECT_URI);
 
-app.listen(3001, () => {
+app.listen(process.env.REDIRECT_URI, () => {
 	console.log("listening...");
 });
 
