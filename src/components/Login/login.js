28f31f06f449511144afbd833363068ca938621d
Yash Joshi <yashjoshi@Yashs-MacBook-Air.local>
2021-05-24 23:38:14 +0530

    prepare deploy

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-const LOGIN_URI =
-	process.env.NODE_ENV !== "production"
-		? "http://localhost:8888/login"
-		: "https://doti-ghoul.herokuapp.com/login";
+const LOGIN_URI = "https://doti-ghoul.herokuapp.com/login";
 
 const url =
 	"https://accounts.spotify.com/authorize?client_id=509490d6f8d64997ad8e2eb63fe621c8&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
